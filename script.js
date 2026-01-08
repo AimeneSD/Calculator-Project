046b9f5749fde9b91ca02f71c8f768f74874a27d
Aimene SAOUD <aimenesaoud@gmail.com>
2026-01-08 14:42:40 +0100

    feat(ux): disable button focus and tab navigation

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
+//1. CONFIGURATION
+
 console.log("Working !");
 const display = document.getElementById("display");
-const operator = ["+","-","*","/"]
+const operators = ["+","-","*","/"]
 
+//2. FONCTIONS D'AFFICHAGE
 function appendToDisplay(input){//AJOUTE DES CARACTERES A CHAQUE APPUI DE BOUTON
     display.blur(); // Enlève le focus de n'importe quel élément pour nettoyer le clavier
     
     const lastchar = display.value.slice(-1)
+    if (operators.includes(input) && operators.includes(lastchar)) {
+        return; // On arrête tout, on n'ajoute pas le deuxième opérateur
+    }
     display.value = display.value + input;
+    
 
-}
-
-function ClearLast(){//SUPPRIME LE DERNIER CARACTERE
-    display.value = display.value.slice(0, -1);
 }
 
 function ClearDisplay(){//SUPPRIME TOUT
     display.value="";
 }
 
+function ClearLast(){//SUPPRIME LE DERNIER CARACTERE
+    display.value = display.value.slice(0, -1);
+}
+
+//3. LOGIQUE DE CALCUL
 function calculate(){//CALCULE SI ET SEULEMENT SI LA SYNTAXE EST CORRECTE SINON AFFICHE "Error"
+    if (display.value==="") return;
+
     try{
-    display.value = eval(display.value);
+        const result= eval(display.value);
+        display.value=Number.isFinite ? result : "Error"
     }catch(error){
         display.value = "Error";
     }
 }
 
+//4. ECOUTEURS D'ÉVENEMENTS
+
 window.addEventListener("keydown", (event) => {
     const key = event.key;
 
-    // Empêche le comportement par défaut (comme cliquer sur le bouton focus)
+    // Empêche le comportement par défaut (comme cliquer sur le bouton focus par le navigateur)
     if (key === "Enter") {
         event.preventDefault(); // <--- TRÈS IMPORTANT
         calculate();
 
-    } else if (/^[0-9]$/.test(key) || ["+", "-", "*", "/"].includes(key)) { // Le ^ signifie "commence par" et le $ signifie "finit par"
+    } else if (/^[0-9]$/.test(key) || operators.includes(key)) { // Le ^ signifie "commence par" et le $ signifie "finit par"
         // Optionnel : preventDefault ici aussi pour éviter les doubles saisies 
         // si un bouton est focus
         event.preventDefault(); 
